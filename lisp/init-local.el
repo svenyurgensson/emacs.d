685d905e150463ff9d8f978b105e01ccade02a28
Yury Batenko <jurbat@gmail.com>
2017-06-12 13:39:48 +0700

    Add eshell PS

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -167,4 +167,22 @@
 (autoload 'rjsx-mode "jsx-mode" "Editing jsx filed" t)
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 
+(setq eshell-prompt-function
+(lambda ()
+(concat
+(propertize "┌─[" 'face `(:foreground "green"))
+(propertize (user-login-name) 'face `(:foreground "red"))
+(propertize "@" 'face `(:foreground "green"))
+(propertize (system-name) 'face `(:foreground "blue"))
+(propertize "]──[" 'face `(:foreground "green"))
+(propertize (format-time-string "%H:%M" (current-time)) 'face `(:foreground "yellow"))
+(propertize "]──[" 'face `(:foreground "green"))
+(propertize (concat (eshell/pwd)) 'face `(:foreground "white"))
+(propertize "]\n" 'face `(:foreground "green"))
+(propertize "└─>" 'face `(:foreground "green"))
+(propertize (if (= (user-uid) 0) " # " " $ ") 'face `(:foreground "green"))
+)))
+
+
+
 (provide 'init-local);;;
