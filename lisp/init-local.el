f97f86400c04666484862222014b2070ac0030b5
Yury Batenko <jurbat@gmail.com>
2019-03-10 16:23:55 +0700

    Add company-tabnine

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -205,4 +205,22 @@
 
 (global-set-key (kbd "C-x ,") 'bjm/align-whitespace)
 
+
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
+
+;; Use the tab-and-go frontend.
+;; Allows TAB to select and complete at the same time.
+(company-tng-configure-default)
+(setq company-frontends
+      '(company-tng-frontend
+        company-pseudo-tooltip-frontend
+        company-echo-metadata-frontend))
+
+
 (provide 'init-local) ;;;
